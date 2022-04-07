f9b2eef8b2f22ab35d9515ba6bbd5449e04204e7
Steven Hicks <steven.hicks@camunda.com>
2022-04-07 10:12:06 -0500

    update aliases. move some things to .git.zsh, remove others.

--- aliases.zsh
+++ aliases.zsh
@@ -1,67 +1,14 @@
-
-alias pgstart="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
-
-alias pgstop="pg_ctl -D /usr/local/var/postgres stop -s -m fast && echo 'postgres server stopped'"
+# leaving these here in case I need them later
+# alias pgstart="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
+# alias pgstop="pg_ctl -D /usr/local/var/postgres stop -s -m fast && echo 'postgres server stopped'"
 
 alias lsg="echo 'npm ls -g --depth=0\n' && npm ls -g --depth=0"
 
-# alias pr="open `git config remote.origin.url`/compare/$(git rev-parse --abbrev-ref HEAD)"
-
-gh(){
-  open $(git config remote.origin.url | sed "s/git@\(.*\):\(.*\).git/https:\/\/\1\/\2/")/$1$2
-}
-
-# Open master in browser
-alias web='gh'
-
-# Open current branch in browser
-alias webbranch='gh tree/$(git symbolic-ref --quiet --short HEAD )'
-
-# Open PR for current branch in browser
-alias pr='gh compare/$(git symbolic-ref --quiet --short HEAD )'
-
-# git aliases
-alias gpo='git push origin'
-alias gpo1='git push --set-upstream origin $(git symbolic-ref --quiet --short HEAD )'
-
-alias gcam='git add . && git commit -am'
-alias gcam!='git add . && git commit --no-verify -am'
-alias gkm='git commit -m'
-alias gkm!='git commit --no-verify -m'
-
-# jrnl aliases
-#alias today='jrnl -from today --export json | jq .entries[].title -r | awk '"'"'{print "- "$0""}'"'"' '
-alias today='jrnl -from today --export json | jq '"'"'.entries[] | "- \(.title) \(.body)" | rtrimstr("\n") | rtrimstr("\n")'"'"' -r '
-
-alias today-slack='today | pbcopy'
-
-alias yesterday='jrnl -from yesterday -to yesterday --export json | jq '"'"'.entries[] | "- \(.title) \(.body)" | rtrimstr("\n") | rtrimstr("\n")'"'"' -r '
-
-alias edit-jrnl='code ~/journal.txt'
-
-alias stashes="git stash list | head -n 5"
-
-# hokusai consoles
-
-alias console-staging="hokusai staging run \"bundle exec rails console\" --tty"
-alias console-prod="hokusai production run \"bundle exec rails console\" --tty"
-
-alias use-xcode-10="sudo xcode-select -s /Applications/XCode10/XCode.app"
-alias use-xcode-11="sudo xcode-select -s /Applications/XCode11/XCode.app"
-alias ios-sim="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
-
-# more memorable name for osx plugin commands
-alias finder="ofd"
-alias finder-cd="cdf"
-
-# bundle aliases
-alias bx="bundle exec"
-alias bxr="bundle exec rspec"
-
+# colorize `ls` output
+alias ls='ls -G'
 # brew aliases
 alias bsl="brew services list"
 alias bs="brew services"
-alias flip_mongo_table="rm /usr/local/var/mongodb/mongod.lock && brew services restart mongodb-community@4.0"
 
 # calendar magic
 alias upcoming="cal -A 2"
