f5adc97124992bef952b87e1704b9b4bc74f0d3a
Steven Hicks <steven.hicks@camunda.com>
2022-04-07 10:57:05 -0500

    .git.zsh - new file for all the git things

---
+++ git.zsh
@@ -0,0 +1,120 @@
+# Open main in browser
+alias web='gh repo view --web'
+
+# Open current branch in browser
+alias webbranch='gh repo view --web --branch $(git symbolic-ref --quiet --short HEAD )'
+
+# Open PR for current branch in browser
+alias pr='gh pr create --web'
+
+# Browse pulls for current repo
+alias pulls='gh pr list --web'
+
+# git aliases
+alias gco='git checkout'
+alias gpo='git push origin'
+alias gpo1='git push --set-upstream origin $(git symbolic-ref --quiet --short HEAD )'
+alias gst='git status'
+
+alias gcam='git add . && git commit -am'
+alias gcam!='git add . && git commit --no-verify -am'
+alias gkm='git commit -m'
+alias gkm!='git commit --no-verify -m'
+
+alias stashes="git stash list | head -n 5"
+
+function branch() {
+  git checkout -b $1
+}
+
+function stash() {
+  git add .
+  if [[ $1 ]] then
+    git stash push -m "$1"
+  else
+    git stash push
+  fi
+}
+
+function unstash() {
+  re='^[0-9]+$'
+  if [[ $1 ]] then
+    if [[ $1 =~ $re ]] then
+      echo "Applying stash@{$1}..."
+      git stash apply stash@{$1}
+    else
+      echo "Applying stash named "$1"..."
+      git stash apply $(git stash list | grep "$1" | cut -d: -f1)
+    fi
+  else
+    echo "Applying most recent stash..."
+    git stash apply
+  fi
+}
+
+function sync() {
+
+  local mainline=$(main_or_master)
+
+  git checkout $mainline
+
+  if [[ `git remote -v | grep upstream` ]]; then
+    echo "syncing to upstream..."
+    git pull upstream $mainline
+    git push origin
+  else
+    echo "syncing to origin..."
+    git pull origin $mainline
+  fi
+}
+
+function main_or_master() {
+  if (branch_exists main); then
+    echo 'main'
+  else
+    echo 'master'
+  fi
+}
+
+function branches() {
+  COUNT=${1:-5}
+  git branch --sort=-committerdate | head -n $COUNT
+}
+
+function branch_exists() {
+    local branch=${1}
+    local exists=$(git branch --list ${branch})
+
+    if [[ -z ${exists} ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+function rebaseonmain() {
+  local mainline=$(main_or_master)
+  if [[ `git status --porcelain` ]]; then
+    local needToStashAndUnstash=true
+  else
+    local needToStashAndUnstash=false
+  fi
+
+  if [[ "$needToStashAndUnstash" = true ]]
+  then
+    stash
+  fi
+
+  echo "syncing $mainline branch to upstream...."
+  sync
+
+  git checkout -
+
+  echo "rebasing on $mainline...."
+  git rebase $mainline
+
+  if [[ "$needToStashAndUnstash" = true ]]
+  then
+    unstash
+  fi
+}
