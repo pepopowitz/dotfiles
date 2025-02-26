e588d516941b44a84c63e58dfb146df4c84e6ad2
Steven Hicks <steven.hicks@camunda.com>
2025-02-25 20:47:22 -0600

    notifications for aerospace save

--- aerospace/aerospace.save.sh
+++ aerospace/aerospace.save.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 $(brew --prefix)/bin/aerospace list-windows --all --format "%{window-id}%{workspace}%{app-bundle-id}%{window-title}" --json > ~/.aerospace/.aerospace.windows.json 
+
+terminal-notifier -message "Window layout saved" -title "Aerospace" -sound "Submarine"
