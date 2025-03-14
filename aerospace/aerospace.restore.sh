8460cc2d1971350ab631429248fa4120410101df
Steven Hicks <steven.j.hicks@gmail.com>
2025-03-13 20:35:00 -0500

    improve aerospace restore

--- aerospace/aerospace.restore.sh
+++ aerospace/aerospace.restore.sh
@@ -7,7 +7,23 @@ if [[ ! -f "$layout_file" ]]; then
   exit 1
 fi
 
-# todo: make this smarter, so that it works after a reboot when window IDs no longer match.
+# grab all open windows.
+current_windows=$($(brew --prefix)/bin/aerospace list-windows --all --format "%{window-id}%{app-bundle-id}%{window-title}" --json)
 
-jq -rc '.[] | "\(.["window-id"]) \(.workspace)"' "$layout_file" |
-  xargs -n2 sh -c '$(brew --prefix)/bin/aerospace move-node-to-workspace --window-id $1 $2 --fail-if-noop' _
+# todo: matching windows for things like code should exclude the open file name.
+
+jq -r --argjson current "$current_windows" '
+  .[] as $saved_window | 
+  ($current[] | select(
+    .["window-id"] == $saved_window["window-id"] or
+    (.["app-bundle-id"] == $saved_window["app-bundle-id"] and .["window-title"] == $saved_window["window-title"])
+  )["window-id"]) as $matched_id |
+  if $matched_id then
+    [$matched_id, $saved_window.workspace] | @tsv
+  else 
+    empty 
+  end
+' "$layout_file" |
+while IFS=$'\t' read -r window_id workspace; do
+  $(brew --prefix)/bin/aerospace move-node-to-workspace --window-id $window_id \"$workspace\" --fail-if-noop
+done
